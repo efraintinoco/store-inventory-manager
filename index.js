e52383333a70293d856a244170887fa7f5d13430
efraintinoco <oakcliff_21@yahoo.com>
2022-04-28 18:13:18 -0500

    add table

--- index.js
+++ index.js
@@ -1,29 +1,92 @@
 const form = document.querySelector("form")
-const input = document.querySelector("input")
-const submit = document.querySelector("submit")
-const article = document.querySelector(".inventory")
+const table = document.querySelector("table")
+const today = new Date().toLocaleDateString("en-US")
+const localStorageItems = localStorage.getItem("existingInventory");
 
-let items = {}
-form.addEventListener("submit", (event) => {
+
+
+
+
+let inventory = [{
+    item: "+5 Dexterity Vest",
+    sellIn: 10,
+    quality: 20,
+    catagory: "none",
+    date: today
+}, {
+    item: "Aged Brie",
+    sellIn: 2,
+    quality: 0,
+    catagory: "aged Brie",
+    date: today
+}, {
+    item: "Elixir of the Mongoose",
+    sellIn: 5,
+    quality: 7,
+    catagory: "none",
+    date: today
+}, {
+    item: "Sulfuras",
+    sellIn: 0,
+    quality: 80,
+    catagory: "sulfuras",
+    date: today
+}, {
+    item: "Backstage passes to a TAFKAL80ETC concert",
+    sellIn: 15,
+    quality: 20,
+    catagory: "backstage pass",
+    date: today
+}]
+
+inventory.map(item => {
+
+    const tr = document.createElement("tr")
+
+    tr.innerHTML = `
+    <td>${item.item}</td>
+    <td>${item.sellIn} days</td>
+    <td>${item.quality}</td>
+    <td>${today}</td>
+    `
+    table.append(tr)
+})
+
+if (localStorageItems) {
+
+    const parsedItems = JSON.parse(localStorageItems)
+    const { existingInventory } = parsedItems
+
+    existingInventory.map((items) => {
+        const createRow = document.createElement("tr")
+        createRow.innerHTML = `
+        <td>${items.item}</td>
+        <td>${items.sellIn} days</td>
+        <td>${items.quality}</td>
+        <td>${today}</td>
+        `
+        table.append(createRow)
+    })
+}
+
+
+form.addEventListener("submit", event => {
     event.preventDefault()
-    const formdata = new FormData(event.target)
-
-    items = {
-        itemName: formdata.get("item-name"),
-        itemSellIn: formdata.get("sell-in"),
-        itemQuality: formdata.get("quality"),
-        itemAdded: formdata.get("date-added"),
-
+    location.reload();
+    const formData = new FormData(event.target)
+    const itemName = formData.get("item-name")
+    const sellIn = formData.get("sell-in")
+    const quality = formData.get("quality")
+    const date = formData.get("date-added")
+    const newInventory = {
+        item: itemName,
+        sellIn: sellIn,
+        quality: quality,
+        date: date
     }
 
-    const listOfInventory = document.createElement("div")
-    article.append(listOfInventory)
-    listOfInventory.innerHTML = `
-    <span>${items.itemName}</span>
-    <span>${items.itemSellIn}</span>
-    <span>${items.itemQuality}</span>
-    <span>${items.itemAdded}</span>
-    `
-    event.target.clear()
-
+    const existingInventory = localStorageItems ? JSON.parse(localStorageItems).existingInventory : []
+    existingInventory.push(newInventory)
+    const itemsJSON = JSON.stringify({ existingInventory })
+    localStorage.setItem("existingInventory", itemsJSON)
 })
